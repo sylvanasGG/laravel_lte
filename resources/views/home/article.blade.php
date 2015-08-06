a457f438153b3d27406f680314c41bfef73db5f3
sylvanas <77849093@qq.com>
2015-08-06 23:32:07 +0800

    *评论调整3

--- resources/views/home/article.blade.php
+++ resources/views/home/article.blade.php
@@ -34,25 +34,31 @@
 
             <div id="comments" style="margin-bottom: 100px;">
 
-                <div class="conmments" >
-                    @foreach($article->comment as $comment)
-                        <div class="one" style="border-top: solid 5px #49B544; padding: 0px 20px;background-color:#ffffff;">
-                            <div class="nickname" data="{{$comment->nickname}}">
+                <div class="conmments" style="background-color:#ffffff;">
+                    <table class="table table-striped">
+                        <colgroup>
+                            <col style="width: 10%" />
+                            <col style="width: 20%" />
+                            <col  />
+                            <col style="width: 10%" />
+                        </colgroup>
+                        {{--<thead>--}}
+                            {{--<tr style="background-color: #49B544">--}}
+                                {{--<th colspan="4" style="text-align: center;color: #ffffff"><h5>评论</h5></th>--}}
+                            {{--</tr>--}}
+                        {{--</thead>--}}
+                       <tbody>
+                       @foreach($article->comment as $comment)
+                        <tr>
+                            <td><div class="nickname" data="{{$comment['nickname']}}"><strong>{{$comment->nickname}}</strong></div></td>
+                            <td>{{$comment->created_at}}</td>
+                            <td style="word-break:break-all">{{$comment->content}}</td>
+                            <td><a href="#new" onclick="reply(this);">回复</a></td>
+                        </tr>
+                           @endforeach
+                       </tbody>
+                    </table>
 
-                                    <h3>{{$comment->nickname}}</h3>
-                                <h6>{{$comment->created_at}}</h6>
-                            </div>
-                            <div class="content">
-                                <p >
-                                    {{$comment->content}}
-                                </p>
-                            </div>
-                            <div class="reply" style="text-align: right; padding: 5px;">
-                                <a href="#new" onclick="reply(this);">回复</a>
-                            </div>
-                        </div>
-
-                        @endforeach
                 </div>
 
                 <div id="new" style="border-top: solid 5px #49B544;margin-top:20px;background-color: #ffffff;padding-bottom: 5px;padding-top: 5px;">
@@ -80,9 +86,10 @@
 
                 <script>
                     function reply(a) {
-                        var nickname = a.parentNode.parentNode.firstChild.nextSibling.getAttribute('data');
-                        var textArea = document.getElementById('newFormContent');
-                        textArea.innerHTML = '@'+nickname+' ';
+                        var obj = $(a);
+                        var nickname = obj.parent().parent().find(".nickname").attr('data');
+                        var teatArea = $("#newFormContent");
+                        teatArea.val('@'+nickname+' ');
                     }
                 </script>
 
